132e7553432597759fba8e53164a8f7cf00b5f2f
Dan Araújo <dansa.rectech@gmail.com>
2022-05-31 00:19:24 -0300

    Implementando states nao aplicado

--- src/main/java/modal/Orcamento.java
+++ src/main/java/modal/Orcamento.java
@@ -13,4 +13,18 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
+
+    public void aplicarDescontoExtra() {
+
+        var valorDoDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE")) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        }
+
+        if (situacao.equals("APROVADO")) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
 }
